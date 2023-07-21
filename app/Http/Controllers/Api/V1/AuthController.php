66e36001fce0fa981fee2cb72ee8fe263a39d088
milanitcc <milan@itccdigital.com>
2023-07-21 11:43:18 +0530

    Some minor changes in database migration and global exception handling

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+/**
+ * @group Authentication
+ *
+ * Class AuthController
+ *
+ * Fullfills all aspects related to authenticate a user.
+ */
+class AuthController extends APIController
+{
+    
+}
