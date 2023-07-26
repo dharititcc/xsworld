c7dc9590fa22fac9e07b66a179a1d6cfa53dcefd
milanitcc <milan@itccdigital.com>
2023-07-26 11:08:56 +0530

    Sample data generation of restaurants

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -43,16 +43,104 @@ class RestaurantSeeder extends Seeder
                 'address'           => '205 Rundle St, Adelaide SA 5000, Australia',
                 'phone'             => '61883100210',
                 'specialisation'    => 'Bar'
-            ]
+            ],
+            [
+                'name'              => 'Shady Pines Saloon',
+                'latitude'          => '-33.8789136',
+                'longitude'         => '151.145561',
+                'address'           => '4/256 Crown St, Darlinghurst NSW 2010, Australia',
+                'specialisation'    => 'Bar'
+            ],
+            [
+                'name'              => 'Employees Only Bar',
+                'latitude'          => '-33.8677238',
+                'longitude'         => '151.1360824',
+                'address'           => '9a Barrack St, Sydney NSW 2000, Australia',
+                'phone'             => '61280847490',
+                'specialisation'    => 'Bar'
+            ],
+            [
+                'name'              => "Grandma's Bar",
+                'latitude'          => '-33.872084',
+                'longitude'         => '151.1354003',
+                'address'           => 'Basement/275 Clarence St, Sydney NSW 2000, Australia',
+                'phone'             => '61292643004',
+                'specialisation'    => 'Bar'
+            ],
+            [
+                'name'              => "Boilermaker House",
+                'latitude'          => '-37.8113429',
+                'longitude'         => '144.8962351',
+                'address'           => '209-211 Lonsdale St, Melbourne VIC 3000, Australia',
+                'phone'             => '61383939367',
+                'specialisation'    => 'Bar'
+            ],
+            [
+                'name'              => "Earl's Juke Joint",
+                'latitude'          => '-33.8999802',
+                'longitude'         => '151.1077377',
+                'address'           => '407 King St, Newtown NSW 2042, Australia',
+                'specialisation'    => 'Bar'
+            ],
+            [
+                'name'              => "Cantina OK!",
+                'latitude'          => '-33.86435432630634',
+                'longitude'         => '151.2047850911538',
+                'address'           => 'Council Pl, Sydney NSW 2000, Australia',
+                'specialisation'    => 'Bar'
+            ],
+            [
+                'name'              => "Cabana Bar",
+                'latitude'          => '-33.86350950341852',
+                'longitude'         => '151.2109746276853',
+                'address'           => '25 Martin Pl, Sydney NSW 2000, Australia',
+                'phone'             => '61292156000',
+                'specialisation'    => 'Bar'
+            ],
+            [
+                'name'              => "Establishment Bar",
+                'latitude'          => '-33.859072608692266',
+                'longitude'         => '151.20828375928136',
+                'address'           => 'Ground Floor, 252 George St, Sydney NSW 2000, Australia',
+                'phone'             => '61291147310',
+                'specialisation'    => 'Bar'
+            ],
+            [
+                'name'              => "Old Mates Place",
+                'latitude'          => '-33.863397796643994',
+                'longitude'         => '151.20480739113526',
+                'address'           => 'level 4/199 Clarence St, Sydney NSW 2000, Australia',
+                'specialisation'    => 'Bar'
+            ],
+            [
+                'name'              => "Molly",
+                'latitude'          => '-35.274696096305654',
+                'longitude'         => '149.12706706250006',
+                'address'           => 'Wooden Door, Odgers Ln, Canberra ACT 2601, Australia',
+                'phone'             => '61261798973',
+                'specialisation'    => 'Bar'
+            ],
+            [
+                'name'              => "The Lobo",
+                'latitude'          => '-33.86584668571258',
+                'longitude'         => '151.20568435944492',
+                'address'           => 'basement lot 1/209 Clarence St, Sydney NSW 2000, Australia',
+                'specialisation'    => 'Bar'
+            ],
         ];
 
         if( !empty( $restaurantArr ) )
         {
+            $counter = 1;
             foreach( $restaurantArr as $restaurant )
             {
                 $newRestaurant = Restaurant::create($restaurant);
 
                 //TODO: logic to upload photo of restaurant
+                $newRestaurant->attachment()->create([
+                    'original_name' => $counter.'.jpg',
+                    'stored_name'   => $counter.'.jpg'
+                ]);
 
                 // Logic related to link item types
                 $newRestaurant->item_types()->attach([1,2,3,4]);
@@ -60,6 +148,8 @@ class RestaurantSeeder extends Seeder
                 // TODO: Logic related to link items
 
                 $newRestaurant->user()->attach(2);
+
+                $counter++;
             }
         }
 
