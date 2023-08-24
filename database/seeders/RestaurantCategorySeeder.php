7e246cff5c23d41f44e17d2c4964f9edaadd54ee
milanitcc <milan@itccdigital.com>
2023-08-24 10:46:33 +0530

    Fixed restaurant category sample data

--- database/seeders/RestaurantCategorySeeder.php
+++ database/seeders/RestaurantCategorySeeder.php
@@ -33,15 +33,246 @@ class RestaurantCategorySeeder extends Seeder
             ]
         ];
 
+        // if( $restaurants->count() )
+        // {
+        //     foreach( $restaurants as $restaurant )
+        //     {
+        //         if( !empty( $categories ) )
+        //         {
+        //             foreach( $categories as $category )
+        //             {
+        //                 $newCategory = $restaurant->categories()->create($category);
+
+        //                 // attachments
+        //                 $newCategory->attachment()->create([
+        //                     'stored_name'   => str_replace(' ', '_', strtolower($category['name'])).'.jpg',
+        //                     'original_name' => str_replace(' ', '_', strtolower($category['name'])).'.jpg'
+        //                 ]);
+        //             }
+        //         }
+        //     }
+        // }
+
         if( $restaurants->count() )
         {
             foreach( $restaurants as $restaurant )
             {
-                if( !empty( $categories ) )
+                if( $restaurant->id == 1 )
                 {
-                    foreach( $categories as $category )
+                    if( !empty( $categories ) )
                     {
-                        $newCategory = $restaurant->categories()->create($category);
+                        foreach( $categories as $category )
+                        {
+                            $newCategory = $restaurant->categories()->create($category);
+
+                            // attachments
+                            $newCategory->attachment()->create([
+                                'stored_name'   => str_replace(' ', '_', strtolower($category['name'])).'.jpg',
+                                'original_name' => str_replace(' ', '_', strtolower($category['name'])).'.jpg'
+                            ]);
+                        }
+                    }
+                }
+
+                if( $restaurant->id == 2 )
+                {
+                    if( !empty( $categories ) )
+                    {
+                        $newCategory = $restaurant->categories()->create($categories[0]);
+
+                        // attachments
+                        $newCategory->attachment()->create([
+                            'stored_name'   => str_replace(' ', '_', strtolower($category['name'])).'.jpg',
+                            'original_name' => str_replace(' ', '_', strtolower($category['name'])).'.jpg'
+                        ]);
+                    }
+                }
+
+                if( $restaurant->id == 3 )
+                {
+                    if( !empty( $categories ) )
+                    {
+                        $newCategory = $restaurant->categories()->create($categories[1]);
+
+                        // attachments
+                        $newCategory->attachment()->create([
+                            'stored_name'   => str_replace(' ', '_', strtolower($category['name'])).'.jpg',
+                            'original_name' => str_replace(' ', '_', strtolower($category['name'])).'.jpg'
+                        ]);
+                    }
+                }
+
+                if( $restaurant->id == 4 )
+                {
+                    if( !empty( $categories ) )
+                    {
+                        foreach( $categories as $category )
+                        {
+                            $newCategory = $restaurant->categories()->create($category);
+
+                            // attachments
+                            $newCategory->attachment()->create([
+                                'stored_name'   => str_replace(' ', '_', strtolower($category['name'])).'.jpg',
+                                'original_name' => str_replace(' ', '_', strtolower($category['name'])).'.jpg'
+                            ]);
+                        }
+                    }
+                }
+
+                if( $restaurant->id == 5 )
+                {
+                    if( !empty( $categories ) )
+                    {
+                        $newCategory = $restaurant->categories()->create($categories[0]);
+
+                        // attachments
+                        $newCategory->attachment()->create([
+                            'stored_name'   => str_replace(' ', '_', strtolower($category['name'])).'.jpg',
+                            'original_name' => str_replace(' ', '_', strtolower($category['name'])).'.jpg'
+                        ]);
+                    }
+                }
+
+                if( $restaurant->id == 6 )
+                {
+                    if( !empty( $categories ) )
+                    {
+                        $newCategory = $restaurant->categories()->create($categories[1]);
+
+                        // attachments
+                        $newCategory->attachment()->create([
+                            'stored_name'   => str_replace(' ', '_', strtolower($category['name'])).'.jpg',
+                            'original_name' => str_replace(' ', '_', strtolower($category['name'])).'.jpg'
+                        ]);
+                    }
+                }
+
+                if( $restaurant->id == 7 )
+                {
+                    if( !empty( $categories ) )
+                    {
+                        foreach( $categories as $category )
+                        {
+                            $newCategory = $restaurant->categories()->create($category);
+
+                            // attachments
+                            $newCategory->attachment()->create([
+                                'stored_name'   => str_replace(' ', '_', strtolower($category['name'])).'.jpg',
+                                'original_name' => str_replace(' ', '_', strtolower($category['name'])).'.jpg'
+                            ]);
+                        }
+                    }
+                }
+
+                if( $restaurant->id == 8 )
+                {
+                    if( !empty( $categories ) )
+                    {
+                        $newCategory = $restaurant->categories()->create($categories[0]);
+
+                        // attachments
+                        $newCategory->attachment()->create([
+                            'stored_name'   => str_replace(' ', '_', strtolower($category['name'])).'.jpg',
+                            'original_name' => str_replace(' ', '_', strtolower($category['name'])).'.jpg'
+                        ]);
+                    }
+                }
+
+                if( $restaurant->id == 9 )
+                {
+                    if( !empty( $categories ) )
+                    {
+                        $newCategory = $restaurant->categories()->create($categories[1]);
+
+                        // attachments
+                        $newCategory->attachment()->create([
+                            'stored_name'   => str_replace(' ', '_', strtolower($category['name'])).'.jpg',
+                            'original_name' => str_replace(' ', '_', strtolower($category['name'])).'.jpg'
+                        ]);
+                    }
+                }
+
+                if( $restaurant->id == 10 )
+                {
+                    if( !empty( $categories ) )
+                    {
+                        foreach( $categories as $category )
+                        {
+                            $newCategory = $restaurant->categories()->create($category);
+
+                            // attachments
+                            $newCategory->attachment()->create([
+                                'stored_name'   => str_replace(' ', '_', strtolower($category['name'])).'.jpg',
+                                'original_name' => str_replace(' ', '_', strtolower($category['name'])).'.jpg'
+                            ]);
+                        }
+                    }
+                }
+
+                if( $restaurant->id == 11 )
+                {
+                    if( !empty( $categories ) )
+                    {
+                        $newCategory = $restaurant->categories()->create($categories[0]);
+
+                        // attachments
+                        $newCategory->attachment()->create([
+                            'stored_name'   => str_replace(' ', '_', strtolower($category['name'])).'.jpg',
+                            'original_name' => str_replace(' ', '_', strtolower($category['name'])).'.jpg'
+                        ]);
+                    }
+                }
+
+                if( $restaurant->id == 12 )
+                {
+                    if( !empty( $categories ) )
+                    {
+                        $newCategory = $restaurant->categories()->create($categories[1]);
+
+                        // attachments
+                        $newCategory->attachment()->create([
+                            'stored_name'   => str_replace(' ', '_', strtolower($category['name'])).'.jpg',
+                            'original_name' => str_replace(' ', '_', strtolower($category['name'])).'.jpg'
+                        ]);
+                    }
+                }
+
+                if( $restaurant->id == 13 )
+                {
+                    if( !empty( $categories ) )
+                    {
+                        foreach( $categories as $category )
+                        {
+                            $newCategory = $restaurant->categories()->create($category);
+
+                            // attachments
+                            $newCategory->attachment()->create([
+                                'stored_name'   => str_replace(' ', '_', strtolower($category['name'])).'.jpg',
+                                'original_name' => str_replace(' ', '_', strtolower($category['name'])).'.jpg'
+                            ]);
+                        }
+                    }
+                }
+
+                if( $restaurant->id == 14 )
+                {
+                    if( !empty( $categories ) )
+                    {
+                        $newCategory = $restaurant->categories()->create($categories[0]);
+
+                        // attachments
+                        $newCategory->attachment()->create([
+                            'stored_name'   => str_replace(' ', '_', strtolower($category['name'])).'.jpg',
+                            'original_name' => str_replace(' ', '_', strtolower($category['name'])).'.jpg'
+                        ]);
+                    }
+                }
+
+                if( $restaurant->id == 15 )
+                {
+                    if( !empty( $categories ) )
+                    {
+                        $newCategory = $restaurant->categories()->create($categories[1]);
 
                         // attachments
                         $newCategory->attachment()->create([
