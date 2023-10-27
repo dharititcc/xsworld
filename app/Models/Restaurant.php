93f905f2e0c10fe5c6e72e289b44562e940c898c
milanitcc <milan@itccdigital.com>
2023-10-27 10:38:35 +0530

    Fixed performance related rating changes in restaurant

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -84,16 +84,6 @@ class Restaurant extends Model
     }
 
     /**
-     * Get all of the ratings for the Restaurant
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough
-     */
-    public function ratings(): HasManyThrough
-    {
-        return $this->hasManyThrough(OrderReview::class, Order::class, 'restaurant_id', 'order_id', 'id', 'id');
-    }
-
-    /**
      * Get all of the restaurant_time for the Restaurant
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
@@ -251,8 +241,7 @@ class Restaurant extends Model
      */
     public function getAverageRatingAttribute()
     {
-        $rating = $this->ratings()->average('rating');
-        return $rating ? $rating : 0;
+        return $this->reviews()->avg('rating');
     }
 
     /**
