dc328ca8cab8b24e8a3290f6b368d2c2e75e165e
Dharit Maniyar <dharit@devsinindia.com>
2023-11-03 15:23:07 +0530

    change text

--- resources/views/emails/giftcard.blade.php
+++ resources/views/emails/giftcard.blade.php
@@ -2,12 +2,9 @@
 
 Hello {{ $name }}!
 
-Please click the button below to verify your email.
-
+Here is a Giftcard code for redeem.
 
 <b>{{ $code }}</b>
-
-If you did not create an account, no further action is required.
 
 Regards,
 <br />
