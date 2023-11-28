833890932d3cf617c8699f99c0ffa70d3a3a620b
Dharit Maniyar <dharit@devsinindia.com>
2023-11-28 11:07:10 +0530

    add faq API

---
+++ app/Http/Controllers/Api/V1/FaqController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Resources\FaqResource;
+use App\Repositories\FaqRepository;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class FaqController extends APIController
+{
+    /** @var \App\Repositories\FaqRepository $repository */
+    protected $repository;
+
+     /**
+     * Method __construct
+     *
+     * @param FaqRepository $repository [explicite description]
+     *
+     * @return void
+     */
+    public function __construct(FaqRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+
+    /**
+     * Method index
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $faq        = $this->repository->getFaq();
+
+        if( $faq->count() )
+        {
+            return $this->respondSuccess('FAQs Found.', FaqResource::collection($faq));
+        }
+
+        return $this->respondWithError('FAQs not found.');
+    }
+}
