d143fe711b608878fc6658141785382a74c03b5f
atharmarfat <athar@devsinindia.com>
2023-11-29 10:17:48 +0530

    bar inprogress order apis

--- app/Repositories/BarRepository.php
+++ app/Repositories/BarRepository.php
@@ -69,6 +69,7 @@ class BarRepository extends BaseRepository
             $query->where('category_id',$category_id)
                 ->where('status', OrderItem::PENDNIG);
         })
+        ->where(['type'=> Order::ORDER])
         // ->where(['type'=> Order::ORDER , 'status' => Order::PENDNIG])
         ->orderBy('id','desc')
         ->get();
