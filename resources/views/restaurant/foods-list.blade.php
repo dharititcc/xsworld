79a75d0a19bf496340048dc4367428185c0b62c7
atharmarfat <athar@devsinindia.com>
2023-12-05 16:16:41 +0530

    variation bugs fixed

--- resources/views/restaurant/foods-list.blade.php
+++ resources/views/restaurant/foods-list.blade.php
@@ -126,7 +126,7 @@
                     <div class="prd-variation" style="display: none">
                         <div class="head">
                             <h2 class="yellow">Food Variations</h2>
-                            <div class="add-remove"><a href="javascript:void(0);" class="bor-btn plus remove_variation" type="button"><i        class="icon-plus"></i></a> <a href="javascript:void(0);" class="bor-btn minus" type="button"><i class="icon-minus"></i></a></div>
+                            {{-- <div class="add-remove"><a href="javascript:void(0);" class="bor-btn plus remove_variation" type="button"><i        class="icon-plus"></i></a> <a href="javascript:void(0);" class="bor-btn minus" type="button"><i class="icon-minus"></i></a></div> --}}
                         </div>
                         <div class="variety grid colmn-7">
                             {{-- <div class="grey-brd-box item-box">
