aa0e934b13cd69a5c9012c162502fc9fba7ac264
atharmarfat <athar@devsinindia.com>
2023-12-07 13:43:38 +0530

    bugs fixed in portal

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Table\RestaurantTableController;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Number;
 
 /*
 |--------------------------------------------------------------------------
