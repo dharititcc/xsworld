05c35339051eed1d456d46b17ce7e0932003f76f
atharmarfat <athar@devsinindia.com>
2023-12-29 10:00:43 +0530

    145 = As discussed ather, Click of Reorder order, only last order pickup order is redirected in to cart

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -998,7 +998,7 @@ class OrderRepository extends BaseRepository
     public function reOrder(array $data): Order
     {
         $user                   = auth()->user();
-        $orderAgain             = $user->orders()->where('restaurant_id', $data['restaurant_id'])->where('type',Order::ORDER)->whereNotIn('status',[Order::CUSTOMER_CANCELED,Order::RESTAURANT_CANCELED,Order::RESTAURANT_TOXICATION])->orderByDesc('id')->first();
+        $orderAgain             = $user->orders()->where('restaurant_id', $data['restaurant_id'])->where('type',Order::ORDER)->whereNotIn('status',[Order::CUSTOMER_CANCELED,Order::RESTAURANT_CANCELED,Order::RESTAURANT_TOXICATION,Order::DENY_ORDER,Order::CURRENTLY_BEING_PREPARED,Order::READYFORPICKUP,Order::COMPLETED])->orderByDesc('id')->first();
 
         $resName = Restaurant::select('name')->where('id',$data['restaurant_id'])->first();
         if( !isset($orderAgain->id) )
@@ -1029,8 +1029,8 @@ class OrderRepository extends BaseRepository
         $newOrder->last_delayed_time    = null;
         $newOrder->remaining_date       = null;
         $newOrder->accepted_date        = null;
-        $newOrder->pickup_point_id      = null;
-        $newOrder->pickup_point_user_id = null;
+        // $newOrder->pickup_point_id      = null;
+        // $newOrder->pickup_point_user_id = null;
         $newOrder->waiter_id            = null;
         $newOrder->served_date          = null;
         $newOrder->completion_date      = null;
