f05374c58a053047b360fabb4be7b6cf8b18e023
milanitcc <milan@itccdigital.com>
2024-01-03 10:17:10 +0530

    Fixed customer status when kitchen update status

--- app/Http/Controllers/Api/V1/Traits/OrderStatus.php
+++ app/Http/Controllers/Api/V1/Traits/OrderStatus.php
@@ -27,7 +27,7 @@ trait OrderStatus
                 if( isset( $order->restaurant_table_id ) )
                 {
                     // update waiter status to Ready for collection
-                    $order->update(['waiter_status' => Order::READY_FOR_COLLECTION]);
+                    $order->update(['waiter_status' => Order::READY_FOR_COLLECTION, 'status' => Order::COMPLETED]);
                 }
             }
 
@@ -41,7 +41,7 @@ trait OrderStatus
                 if( isset( $order->restaurant_table_id ) )
                 {
                     // update waiter status to Ready for collection
-                    $order->update(['waiter_status' => Order::CURRENTLY_BEING_SERVED]);
+                    $order->update(['waiter_status' => Order::CURRENTLY_BEING_SERVED, 'status' => Order::CONFIRM_PICKUP]);
                 }
             }
         }
