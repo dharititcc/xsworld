6bfecb696e6a312692aed02ceeab9fca3eefe221
Dharit Maniyar <dharit@devsinindia.com>
2024-01-05 15:58:56 +0530

    working on key insights

---
+++ resources/views/restaurant/key_insights.blade.php
@@ -0,0 +1,12 @@
+<tr>
+    <th>Total Tables</th>
+    <td> {{ $key_insights['total_tables'] }} </td>
+</tr>
+<tr>
+    <th>Active Tables</th>
+    <td> {{ $key_insights['active_tbl'] }} </td>
+</tr>
+<tr>
+    <th>Occupied Tables</th>
+    <td> {{ $key_insights['occupied_tbl'] }} </td>
+</tr>
