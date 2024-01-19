70de33d72af41a85469192093ef8f289badb0f07
atharmarfat <athar@devsinindia.com>
2024-01-19 13:01:26 +0530

    gift credit amount apis

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -1292,7 +1292,12 @@ class OrderRepository extends BaseRepository
     {
         $auth_user = auth()->user();
         $friend = User::find($data['user_id']);
+        $checkFriend    = FriendRequest::where('friend_id',$auth_user->id)->where('user_id',$data['user_id'])->count();
+        if($checkFriend > 1) {
+            throw new GeneralException('Already send Friend request'); 
+        }
         $auth_user->friends()->attach($friend->id);
+
         // $FriendRequest = FriendRequest::create([
         //     'user_id'   => $auth_user->id,
         //     'friend_id' => $data['user_id'],
@@ -1354,11 +1359,11 @@ class OrderRepository extends BaseRepository
         $amount                 = $data['amount'];
         $paymentArr = [
             'amount'        => number_format($amount, 2) * 100,
-            'currency'      => $auth_user->orders->restaurant->currency->code,
+            // 'currency'      => $auth_user->orders->restaurant->currency->code,
+            'currency'      => 'aud',
             'customer'      => $auth_user->stripe_customer_id,
-            'capture'       => false,
             'source'        => $data['card_id'],
-            'description'   => $auth_user->id
+            'description'   => "Gift Credit Send to ". $data['user_id']
         ];
         $stripe         = new Stripe();
         $payment_data   = $stripe->createCharge($paymentArr);
