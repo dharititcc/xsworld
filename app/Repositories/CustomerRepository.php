8716c906c1a0918437b4c3b5cdc6c255e3f47a1f
atharmarfat <athar@devsinindia.com>
2024-01-24 10:21:36 +0530

    admin portal bugs fixed & remove self user data in vneue user list apis

--- app/Repositories/CustomerRepository.php
+++ app/Repositories/CustomerRepository.php
@@ -13,9 +13,10 @@ class CustomerRepository extends BaseRepository
     */
     const MODEL = User::class;
 
-    public function getCustomerForDatatable()
+    public function getCustomerForDatatable(array $data)
     {
         $active = isset($input['active']) ? $input['active'] : 0;
+        $search = $data['search_main'];
         $query = $this->query()
             ->select([
                 'users.id',
@@ -29,7 +30,11 @@ class CustomerRepository extends BaseRepository
         {
             $query->onlyTrashed();
         }
-
+        if($search)
+        {
+            $query =  $query->where('users.first_name' , 'LIKE', '%'.$search.'%')
+                            ->orWhere('users.last_name', 'LIKE', '%'.$search.'%');
+        }
         return $query;
     }
 }
