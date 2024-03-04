3e862a648ad5e3494d5f67fc2818e855f4fb4364
gamitbrijesh9 <brijesh@confabure.com>
2024-03-04 14:51:06 +0530

    css stle changes

--- resources/views/emails/giftcard.blade.php
+++ resources/views/emails/giftcard.blade.php
@@ -10,10 +10,9 @@
 <body class="home-wrap" style="margin: 0; padding: 0; font-family: Arial, sans-serif;">
 
     <div class="main" style="max-width: 1600px; margin: 40px auto; position:relative;">
-        <div class="bg-img" style="position: absolute;top: 14rem;left: 50%;transform: translateX(-50%);height: 255px;width: 519px;z-index: 0;">
-            <img src="{{ asset('vector_bg.png')}}" alt="vector bg" style="height: 100%; width: 100%;">
-        </div>
-        <table class="table" style="width: 100%; border-collapse: collapse; text-align: center;">
+        <img src="{{ asset('vector_bg.png')}}" alt="vector bg" style="height: 100%; width: 100%;position: absolute;top: 13rem;left: 50%;transform: translateX(-50%);height: 255px;width: 519px;z-index: 0; ">
+        <table class="table" style="width: 100%; border-collapse: collapse; text-align: center;z-index: 1;
+    position: relative;">
             <tbody>
                 <tr>
                     <td colspan="12" style="text-align: left;"><img class="logo-img" width="119" height="28" src="{{ asset('XSWorld.png') }}" style="object-fit: contain; padding-left: 20px;" alt="XS World Logo"></td>
@@ -31,13 +30,13 @@
                     <td colspan="12" style="display: flex; justify-content: center; align-items: center; width: 100%;">
                         <div class="boxone" style="border-radius: 12px; border: 0.25px solid  #805711; background: linear-gradient(0deg, rgba(0, 0, 0, 0.80) 0%, rgba(0, 0, 0, 0.80) 100%), linear-gradient(180deg, rgba(255, 255, 255, 0.12) 0%, rgba(254, 243, 179, 0.12) 100%); box-shadow: 0px 0px 17.2px -3px rgba(204, 178, 96, 0.28); backdrop-filter: blur(22px); height: 169px; margin: 50px auto 0; max-width: 308px; padding: 9px; color: #fff; width: 100%;">
                             <div style="display: flex;justify-content: space-between;align-items: center;">
-                                <div><img src="{{ asset('xsworld_gift.png') }}" alt="XS World gift"></div>
-                                <div>
+                                <div style="width: 50%;text-align: left;"><img src="{{ asset('xsworld_gift.png') }}" alt="XS World gift"></div>
+                                <div style="width: 50%;text-align: left;">
                                     <p style="margin-bottom: 0; color: #FFF; text-align: right; font-size: 13px; font-style: normal; font-weight: 400; line-height: normal;">Gift Card</p>
                                 </div>
                             </div>
                             <div style="position: relative;border-radius: 12px;border: 0.25px solid #805711;background: linear-gradient(180deg, rgba(255, 255, 255, 0.12) 0%, rgba(254, 243, 179, 0.12) 100%);box-shadow: 0px 0px 75.7px -28px #FEF3B3;display: flex;justify-content: center;align-items: center;height: 72px;width: 72px;margin: 20px auto 15px;" class="boxone-inner">
-                                <img src="{{ asset('gold.png') }}" style="width: 42px; height: 49px;" alt="vector bg">
+                                <img src="{{ asset('gold.png') }}" style="width: 42px; height: 49px;margin: auto;" alt="vector bg">
                             </div>
                             <div style="display: flex; align-items: center;gap:75px" class="receipent-name">
                                 <div class="name-title" style="width: 46%;">
@@ -53,7 +52,7 @@
                 </tr>
                 <tr>
                     <td colspan="12" style="display: flex; justify-content: center; align-items: center; width: 100%; justify-content: center;">
-                        <div class="amount-box" style="background: linear-gradient(0deg, rgba(0, 0, 0, 0.80) 0%, rgba(0, 0, 0, 0.80) 100%); border-radius: 12px; max-width: 500px; padding: 56px 5vw; color: #fff; width: 100%; min-height: 137px;">
+                        <div class="amount-box" style="background: linear-gradient(0deg, rgba(0, 0, 0, 0.80) 0%, rgba(0, 0, 0, 0.80) 100%); border-radius: 12px; max-width: 500px; padding: 56px 5vw; color: #fff; width: 100%; min-height: 137px;margin: auto;">
                             <p style="text-align: center; font-size: 24px; font-weight: 400; margin: 0; color: #fff;">{{$amount}}</p>
                         </div>
                     </td>
